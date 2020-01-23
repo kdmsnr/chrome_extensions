11cb075fcf1be05fcde032f3e6a1d84904ddb7a7
Masanori Kado <kdmsnr@gmail.com>
2020-01-24 03:41:01 +0900

    Add prefer-kindle-device

---
+++ prefer-kindle-device/content.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", main, false);
+
+function main(e) {
+  const jsInitCheckTimer = setInterval(jsLoaded, 1000);
+  function jsLoaded() {
+    var t = document.querySelector(".ebookBuyboxDeliverToDropdown").children[1];
+    if (t != null) {
+      clearInterval(jsInitCheckTimer);
+      var c = t.parentNode.children;
+      for (var i = 0; i < c.length; i++) {
+        if (c[i].label == "_iPad") {
+          c[i].selected = true;
+          document.querySelector(".a-dropdown-prompt").innerText = '_iPad';
+        }
+      }
+    }
+  }
+}
